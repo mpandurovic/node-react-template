71f5856b4bdc2e5fa8320314d034fd8eb51efdfa
Marko Pandurovic <marko.istimd@gmail.com>
2018-09-27 23:05:44 +0200

    Add BE babel support, modify startup scripts

--- src/server/index.js
+++ src/server/index.js
@@ -1,10 +1,10 @@
 const express = require('express');
-const os = require('os');
+import os from 'os';
 
 const app = express();
 
 app.use(express.static('dist'));
 app.get('/api/hello', (req, res) =>
-    res.send({ hello: true })
+    res.send({ hello: 'reload test' })
 );
 app.listen(8080, () => console.log('Listening on port 8080!'));
