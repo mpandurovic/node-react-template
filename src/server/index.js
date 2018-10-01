357cdbe35e0f5d830288b1f1e9779aa90ea23a28
Marko Pandurovic <marko.istimd@gmail.com>
2018-10-01 22:46:42 +0200

    add eslint

--- src/server/index.js
+++ src/server/index.js
@@ -1,10 +1,11 @@
-const express = require('express');
-import os from 'os';
+import express from 'express';
 
 const app = express();
 
 app.use(express.static('dist'));
-app.get('/api/hello', (req, res) =>
-    res.send({ hello: 'reload test' })
-);
+app.get('/api/hello', (req, res) => res.send({
+  hello: 'reload test',
+}));
+
+// eslint-disable-next-line no-console
 app.listen(8080, () => console.log('Listening on port 8080!'));
