530f939708664b9090bfa014346ab233b7b41d45
Marko Pandurovic <marko.istimd@gmail.com>
2018-10-22 00:34:53 +0200

    update eslint import configuration

--- src/client/containers/auth/Signup.js
+++ src/client/containers/auth/Signup.js
@@ -2,24 +2,20 @@ import React from 'react';
 import { Form, FormGroup, Label, Input, Button } from 'reactstrap';
 // import Button from '../../components/Buton/Button';
 
-class Signup extends React.Component {
-  render() {
-    return (
-      <div className="row justify-content-md-center">
-        <Form>
-          <FormGroup>
-            <Label htmlFor="signup-email">Email:</Label>
-            <Input field="email" id="signup-email" />
-          </FormGroup>
-          <FormGroup>
-            <Label htmlFor="signup-password">Password:</Label>
-            <Input type="password" field="password" id="signup-password" />
-          </FormGroup>
-          <Button className="btn-block" type="submit">Submit</Button>
-        </Form>
-      </div>
-    );
-  }
-}
-
-export default Signup;
+export default () => (
+  <div className="row justify-content-md-center">
+    <Form>
+      <FormGroup>
+        <Label htmlFor="signup-email">Email:</Label>
+        <Input field="email" id="signup-email" />
+      </FormGroup>
+      <FormGroup>
+        <Label htmlFor="signup-password">Password:</Label>
+        <Input type="password" field="password" id="signup-password" />
+      </FormGroup>
+      <Button className="btn-block" type="submit">
+        Submit
+      </Button>
+    </Form>
+  </div>
+);
