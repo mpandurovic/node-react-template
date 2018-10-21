9455ea79567a23999253ffc3bbf7c999f9463b33
Marko Pandurovic <marko.istimd@gmail.com>
2018-10-22 00:29:36 +0200

    Experiment with forms

--- src/client/containers/auth/Signup.js
+++ src/client/containers/auth/Signup.js
@@ -1,12 +1,22 @@
 import React from 'react';
-import Button from '../../components/Buton/Button';
+import { Form, FormGroup, Label, Input, Button } from 'reactstrap';
+// import Button from '../../components/Buton/Button';
 
 class Signup extends React.Component {
-
   render() {
     return (
-      <div>
-        <Button label="Signup"/>
+      <div className="row justify-content-md-center">
+        <Form>
+          <FormGroup>
+            <Label htmlFor="signup-email">Email:</Label>
+            <Input field="email" id="signup-email" />
+          </FormGroup>
+          <FormGroup>
+            <Label htmlFor="signup-password">Password:</Label>
+            <Input type="password" field="password" id="signup-password" />
+          </FormGroup>
+          <Button className="btn-block" type="submit">Submit</Button>
+        </Form>
       </div>
     );
   }
